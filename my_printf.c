c15d57b99a09c1790335eec3a16fc838c18360ea
Mosesomo <moseswamboga4@gmail.com>
2023-06-26 05:00:23 -0700

    changes

--- my_printf.c
+++ my_printf.c
@@ -7,10 +7,10 @@
  *Return: chars printed
  */
 
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
 	
-	unsigned int i = 0, val = 0;
+	unsigned int i = 0, count = 0, val = 0;
 	va_list list;
 
 	va_start(list, format);
