7587d258648ffee179302fd2509dd553aef2ab88
Mosesomo <moseswamboga4@gmail.com>
2023-06-27 17:20:57 -0700

    printf

--- choice.c
+++ choice.c
@@ -25,23 +25,16 @@ int choice(va_list r, char str)
 			count += _putchar('%');
 			break;
 		case 'd':
-			count += printint(va_arg(r, int));
-			break;
 		case 'i':
 			count += printint(va_arg(r, int));
 			break;
 		case '\0':
 			return (-1);
 		default:
-<<<<<<< HEAD
-			count += _putchar('%');
-			count += _putchar(str);
-=======
 			_putchar('%');
 			count++;
 			_putchar(str);
 			count++;
->>>>>>> fbf807b4416ffc7872d78376c36626b151a9c31f
 			break;
 	}
 	return (count);
