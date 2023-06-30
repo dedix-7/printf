f7c0f6b5f270c3eb809f3449334d5f5d093f6ffd
enox <anzimboingosi@gmail.com>
2023-06-29 18:19:02 -0700

    fix betty coding style

--- choice.c
+++ choice.c
@@ -29,7 +29,7 @@ int choice(va_list r, char str)
 			count += printint(va_arg(r, int));
 			break;
 		case 'u':
-			count +=printint(va_arg(r, unsigned int));
+			count += printint(va_arg(r, unsigned int));
 			break;
 		case 'b':
 			count += printbin(va_arg(r, unsigned int));
